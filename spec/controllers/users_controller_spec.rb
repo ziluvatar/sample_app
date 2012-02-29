fd0bb9705ecf3508b3a68dfc1c6ec2c16a60b91a
Eduardo Díaz <eduardo.ds@gmail.com>
2012-02-29 22:44:52 +0100

    Ejercicios capitulo 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,26 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign up")
     end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end    
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end    
+
+    it "should have a password confirmation field" do    
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+    end    
   end
   
   describe "GET 'show'" do
@@ -72,6 +92,17 @@ describe UsersController do
         post :create, :user => @attr
         response.should render_template('new')
       end
+      
+      it "should reset the password field" do
+        post :create, :user => @attr.merge({:password => "aaa", :password_confirmation => "aaa" })
+        response.should have_selector("input[name='user[password]'][value='']")
+      end      
+      
+      it "should reset the password_confirmation field" do
+        post :create, :user => @attr.merge({:password => "aaa", :password_confirmation => "aaa" })
+        response.should have_selector("input[name='user[password_confirmation]'][value='']")
+      end      
+
     end
     
     describe "success" do
